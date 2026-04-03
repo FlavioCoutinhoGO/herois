621342f565bd11d18aacac7589d74917461a9ebd
Flávio Coutinho <coutinho.fg@gmail.com>
2026-04-03 20:23:21 +0000

    fix: IDs dos heróis + tema bandeira brasileira

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,7 +2,7 @@ import { defineCollection } from 'astro:content';
 import { glob } from 'astro/loaders';
 
 const herois = defineCollection({
-  loader: glob({ pattern: "**/*.md", base: "./src/content/herois" })
+  loader: glob({ pattern: "**/*.{md,mdx}", base: "./src/content/herois" })
 });
 
 export const collections = { herois };
